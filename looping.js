e8d033f08e9cbc6024688debbd65dac1bbbf653f
tommybun97 <tommybun97@gmail.com>
2021-05-07 11:04:14 +0700

    bikin function

---
+++ looping.js
@@ -0,0 +1,24 @@
+
+
+
+looping = () => {
+    for(i=0; i=5; i++){
+        console.log(`Hello World`)
+    }
+}
+
+// hitung (3 ,4){
+//     return a*b
+// }
+
+
+let a = 3
+let b = 4
+function hitung(a, b) {
+    console.log(a+b)
+  }
+
+
+let c = 1
+let d = 2
+console.log(a+b)
